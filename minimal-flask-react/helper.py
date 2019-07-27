0b2936d266459e1302f0c08f7a0382de488af3fc
Yuyi Qu <46606152+quyuyi@users.noreply.github.com>
2019-07-27 00:10:38 -0400

    change preprocessing/add iterative results

--- minimal-flask-react/helper.py
+++ minimal-flask-react/helper.py
@@ -18,43 +18,6 @@ np.random.seed(2018)
 import nltk
 nltk.download('wordnet')
 
-# glovefile {'glove.6B.50d.txt','glove.6B.100d.txt','glove.6B.200d.txt'，'glove.6B.300d.txt'}
-def loadGloveModel(gloveFile):
-    print("Loading Glove Model")
-    f = open(gloveFile,'r',encoding='utf-8')
-    model = {}
-    for line in f:
-        splitLine = line.split()
-        word = splitLine[0]
-        embedding = np.array([float(val) for val in splitLine[1:]])
-        model[word] = embedding
-    print("Done.",len(model)," words loaded!")
-    return model
-
-def load_glove_to_word2vec(gloveFile):
-    glove_input_file=gloveFile
-    word2vec_output_file=gloveFile+'.word2vec'
-    glove2word2vec(glove_input_file,word2vec_output_file)
-
-# text preprocessing
-def preprocess_text(texts):
-    stop_words=set(stopwords.words('english'))
-    cleaned_texts=[]
-    lemmatizer=WordNetLemmatizer()
-    for text in texts:
-        # tokens=word_tokenize(text)
-        tokens=re.split('[\-\s\\\+]',text)
-        tokens=[w.lower() for w in tokens]
-        # remove punctuation from each word
-        table = str.maketrans('', '', string.punctuation)
-        stripped = [w.translate(table) for w in tokens]
-        # remove remaining tokens that are not alphabetic
-        words = [word for word in stripped if word.isalpha()]
-        # remove stop words
-        # lemmatization? -> lemmatizer.lemmatize(w)
-        filtered=[w for w in words if not w in stop_words]
-        cleaned_texts+=[filtered]
-    return cleaned_texts
 
 # return a glove word2vec model
 # using pretrained 50d as default glovefile
@@ -66,62 +29,112 @@ def glove_model(glovefile='glove.6B.50d.txt'):
     model=KeyedVectors.load_word2vec_format(glovefile+'.word2vec',binary=False)
     return model
 
-# word embedding at one word level
-def glove_word_embedding(word,model):
-    try:
-        word_embedding=model[word]
-    except KeyError:
-        print('%s not in vocabulary' % word)
-        word_embedding=0
-    return word_embedding
-
-# word embedding at one sentence level
-# take averag
-def sent_vectorizer(sent,model):
-    sent_vec=[]
-    numw=0
-    for w in sent:
-        try:
-            word_embedding=model[w]
-        except KeyError:
-            # print('%s not in vocabulary' % w)
-            word_embedding=0
-        try:
-            if numw==0:
-                sent_vec=word_embedding
-            else:
-                sent_vec=np.add(sent_vec,word_embedding)
-            numw+=1
-        except:
-            pass
-    return sent_vec/numw
 
 
-def glove_vectorizer(texts,model):
-    cleaned_texts=preprocess_text(texts)
-    X=[]
-    for t in cleaned_texts:
-        X.append(sent_vectorizer(t,model))
-    return np.array(X)
+'''
+# alternative way to load glove pretrained model
+import gensim.downloader as api
+def load_model():
+    word_vectors = api.load("glove-wiki-gigaword-100")  # load pre-trained word-vectors from gensim-data
+'''    
 
 
 
-def lemmatize_stemming(sentence):
-    stemmer=SnowballStemmer('english')
-    return stemmer.stem(WordNetLemmatizer().lemmatize(sentence,pos='v'))
 
-def preprocess_sentence(sentence):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+from nltk.corpus import wordnet
+def get_wordnet_pos(treebank_tag):
+    if treebank_tag.startswith('J'):
+        return wordnet.ADJ
+    elif treebank_tag.startswith('V'):
+        return wordnet.VERB
+    elif treebank_tag.startswith('N'):
+        return wordnet.NOUN
+    elif treebank_tag.startswith('R'):
+        return wordnet.ADV
+    else:
+        return wordnet.NOUN
+
+
+def split_document_to_tokens(text):
+    splitter=nltk.data.load('tokenizers/punkt/english.pickle')
+    sentences=splitter.tokenize(text)
+    tokenizer=nltk.tokenize.TreebankWordTokenizer()
+    tokens=[tokenizer.tokenize(sent) for sent in sentences]
+    return tokens
+
+nltk.download('averaged_perceptron_tagger')
+def lemmatization_with_pos(tokens):
+    lemmatizer=WordNetLemmatizer()
+    pos_tokens=[nltk.pos_tag(token) for token in tokens]
     result=[]
-    for token in gensim.utils.simple_preprocess(sentence):
-        if token not in gensim.parsing.preprocessing.STOPWORDS and len(token)>3:
-            result.append(lemmatize_stemming(token))
+    for sent_token in pos_tokens:
+        for pos in sent_token:
+            result+=[lemmatizer.lemmatize(pos[0],get_wordnet_pos(pos[1]))]
     return result
 
-def preprocess(corpus):
+
+import string
+# corpus -> document -> sentence -> token -> POS -> lemma
+def preprocess_using_nltk(corpus):
+    punctuations=string.punctuation
+    stopwords=gensim.parsing.preprocessing.STOPWORDS
     result=[]
-    for sentence in corpus:
-        result.append(preprocess_sentence(sentence))
+    for text in corpus:
+        tokens=split_document_to_tokens(text)
+        lemma=lemmatization_with_pos(tokens)
+        temp=[]
+        for token in lemma:
+            if token not in stopwords and token not in punctuations:
+                temp+=[token.lower()]
+        result+=[temp]
     return result
+
+
+import spacy
+# need download en model
+# python -m spacy download en
+from nltk.corpus import stopwords
+def preprocess_using_spacy(corpus):
+    nlp = spacy.load('en_core_web_sm')
+    punctuations = string.punctuation
+    stopword = stopwords.words('english')
+    texts = []
+    for doc in corpus:
+        doc = nlp(doc, disable=['parser', 'ner'])
+        tokens = [tok.lemma_.lower().strip() for tok in doc if tok.lemma_ != '-PRON-']
+        tokens = [tok for tok in tokens if tok not in stopword and tok not in punctuations and tok.isalpha()]
+        # tokens = ' '.join(tokens)
+        texts.append(tokens)
+    return texts
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def create_dictionary(processed_corpus):
@@ -153,22 +166,53 @@ def create_tfidf_model(dictionary,processed_corpus):
     corpus_tfidf=tfidf[bow_corpus]
     return corpus_tfidf
 
-def keywords_by_lda(corpus_tfidf,dictionary):
-    num_topics=5
+def keywords_by_lda(corpus_tfidf,dictionary,num_topics=10,num_terms=20):
     lda_model_tfidf=gensim.models.LdaMulticore(corpus_tfidf,num_topics=num_topics,id2word=dictionary,passes=2,workers=2)
     
     top_keywords=[]
-    topn=10 # Number of the most significant words that are associated with the topic
     for i in range(0,num_topics):
-        similar=[pair[0] for pair in lda_model_tfidf.show_topic(i,topn=topn)]
+        similar=[pair[0] for pair in lda_model_tfidf.show_topic(i,topn=num_terms)]
         top_keywords+=[similar]
     return top_keywords
 
 
 
+
+# TODO
+# top_seeds are id array not word array???
+def keywords_by_frequency(model,dictionary,num_topics=10,num_terms=20):
+    import operator
+    dfs=dictionary.dfs
+    sorted_dict = sorted(dfs.items(), key=operator.itemgetter(1),reverse=True)
+    top_seeds=[dictionary[item[0]] for item in sorted_dict[0:num_topics]]
+
+    # for each seed
+    # find #num_terms similar words to it using glove and cosine similarity
+    top_keywords=[]
+    for word in top_seeds:
+        similar=[word]
+        similar+=[item[0] for item in model.similar_by_word(word)[:num_terms]]
+        top_keywords+=[similar]
+    return top_keywords
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
 # Select top k frequent keywords 
 # using tfidf
-def keywords_by_frequency(corpus,model,vectorizer,X):
+def keywords_by_frequency_using_sklearn(model,vectorizer):
     indices=np.argsort(vectorizer.idf_)[::-1]
     feature_names=vectorizer.get_feature_names()
     top_k=10
@@ -185,29 +229,40 @@ def keywords_by_frequency(corpus,model,vectorizer,X):
         top_keywords+=[similar]
     return top_keywords
 
-def keywords_to_docs_by_lda(terms,corpus,dictionary,corpus_tfidf,threshold=0):
-    scores=[]
-    for i in range(0,len(corpus_tfidf)):
-        document=corpus_tfidf[i]
-        matched=0
-        summation=0
-        for term in terms:
-            tfidf=0
-            for idx,tfidf_score in document:
-                if (dictionary[idx]==term):
-                    matched+=1
-                    tfidf=tfidf_score
-                    break
-            summation+=tfidf
-        score=matched*summation
-        scores+=[score]
-    top_docs=[]
-    for i in range(0,len(corpus_tfidf)):
-        if (scores[i]>threshold):
-            top_docs+=[corpus[i]]
-    return top_docs
 
-def keywords_to_docs_by_frequency(terms,corpus,vectorizer,X,threshold=0):
+
+
+def select_top_keywords_using_sklearn(corpus,method):
+    if (method=='semantic modeling'):
+        model=glove_model() #slow and unchanged should load only once
+        vectorizer=TfidfVectorizer(stop_words='english',max_features=20000)
+        X=vectorizer.fit_transform(corpus) # need to preprocess corpus?
+        keywords=keywords_by_frequency_using_sklearn(model,vectorizer)
+        docs=[]
+        for terms in keywords:
+            doc=keywords_to_docs_by_frequency(terms,corpus,vectorizer,X,threshold=0)
+            docs+=[doc]
+        return keywords,docs
+
+    # using LDA to create the keyword sets
+    # where each set just corresponds to each topic extracted by LDA
+    elif (method=='topic modeling'):
+        processed_corpus=preprocess_using_spacy(corpus)
+        dictionary=create_dictionary(processed_corpus)
+        corpus_tfidf=create_tfidf_model(dictionary,processed_corpus)
+        keywords=keywords_by_lda(corpus_tfidf,dictionary)
+        docs=[]
+        for terms in keywords:
+            doc=keywords_to_docs_by_lda(terms,corpus,dictionary,corpus_tfidf,threshold=0)
+            docs+=[doc]
+        return keywords,docs
+
+    else:
+        print("Choose method from {'semantic modeling','topic modeling'}")
+
+
+
+def keywords_to_docs_using_sklearn(terms,corpus,vectorizer,X,threshold=0):
     scores=[]
     for i in range(0,len(corpus)):
         document=corpus[i]
@@ -229,3 +284,36 @@ def keywords_to_docs_by_frequency(terms,corpus,vectorizer,X,threshold=0):
         if (scores[i]>threshold):
             top_docs+=[corpus[i]]
     return top_docs
+'''
+
+
+
+
+
+
+
+
+
+
+def write_to_txt(data,filename='test_result.text'):
+    outfile=open(filename,'a')
+
+    for item in data:
+        outfile.write('Keywords:\n')
+        for word in item['words']:
+            outfile.write(word)
+            outfile.write(' ')
+        outfile.write('\n')
+        outfile.write('Matched documents:\n')
+        for doc in item['documents']:
+            outfile.write(doc)
+            outfile.write('\n')
+        '''
+        outfile.write('Documents indices:\n')
+        for idx in item['indices']:
+            outfile.write(str(idx))
+            outfile.write(' ')
+        '''
+        outfile.write('\n\n')
+    
+    outfile.close()
--- minimal-flask-react/server.py
+++ minimal-flask-react/server.py
@@ -20,48 +20,70 @@ def index():
 def suggest_rules():
     data = []
 
+    
     # Retrieve data from database
-    fetch_num=10
-    corpus=retrieve_data(fetch_num)
+    corpus=retrieve_data(
+        # fetch_num=100
+    )
+
+    # process data
+    processed_corpus=preprocess_using_spacy(corpus)
+    
+    dictionary=create_dictionary(processed_corpus)
+    corpus_tfidf=create_tfidf_model(dictionary,processed_corpus)
 
     # Convert data to word embedding 
 
     # Select top keywords (1. frequency 2. LDA)
-    method='topic modeling'
-    # method='semantic modeling' # choose from {'semantic modeling','topic modeling'}
-    keywords,docs=select_top_keywords(corpus,method)
+    methods = ['semantic modeling','topic modeling']
+    method=methods[0]
+    if (method==methods[0]):
+        model=glove_model()
+    else:
+        model=None
+    keywords=select_top_keywords(dictionary,corpus_tfidf,method,model=model,num_topics=5,num_terms=20)
 
     # Match documents by keywords
-    for keyword,doc in zip(keywords,docs):
+    for terms in keywords:
+        doc_list=keywords_to_doc_list(terms,dictionary,corpus_tfidf,sub_indices='default',threshold=0)
+        doc=keywords_to_docs(corpus,doc_list)[:5]
         item={
-            'words':keyword,
-            'documents':doc
+            'words':terms,
+            'documents':doc,
+            'indices':indices
         }
-        data.append(item)
+        data.append(item)        
 
-    print(data)
+    write_to_txt(data,filename='result.txt')
+
+    return data
     # return jsonify(**data)
 
 
-
+# TODO
+# is it necessary to store processed corpus into database
+# what makes this slow? loading from database or preprocessing?
 # Retrieve data from database
+# TODO
+# input: a list of document ids to retrieve
 def retrieve_data(fetch_num='default'): # fetch a small part of the dataset for test
     client = MongoClient('mongodb://user1:user111@ds351987.mlab.com:51987/heroku_vbl2phnb')
     db = client.heroku_vbl2phnb  # use a database called heroku_vbl2phnb
     collection = db.news   # and inside that DB, a collection called news
 
     if (fetch_num=='default'):
-        fetch_num=collection.count()
+        fetch_num=7600 # collection.count()=7600
 
+    cursor=collection.find()
     news=[]
-    for i in range(0,fetch_num):
-        cursor=collection.find()[i]
-        content=cursor['content']
+    for item in cursor:
+        content=item['content']
         news.append(content)
     return news
 
 
-# TODO not used
+# TODO 
+# not used?
 # Convert text to word embedding 
 # input: data in text
 # output: data in vectors
@@ -75,61 +97,123 @@ def keyword_to_embedding(keyword,model):
 
 
 
+# TODO
+# load glove model is slow but we only need to load once
+# when and where to load?
+
 # Select top keywords (1. frequency 2. LDA)
 # input: documents
 # output: keywords
-def select_top_keywords(corpus,method):
-    if (method=='semantic modeling'):
-        model=glove_model() #slow and unchanged should load only once
-        vectorizer=TfidfVectorizer(stop_words='english',max_features=20000)
-        X=vectorizer.fit_transform(corpus) # need to preprocess corpus?
-        keywords=keywords_by_frequency(corpus,model,vectorizer,X)
-        docs=[]
-        for terms in keywords:
-            doc=keywords_to_docs_by_frequency(terms,corpus,vectorizer,X,threshold=0)
-            docs+=[doc]
-        return keywords,docs
-
-    # using LDA to create the keyword sets
-    # where each set just corresponds to each topic extracted by LDA
-    elif (method=='topic modeling'):
-        processed_corpus=preprocess(corpus)
-        dictionary=create_dictionary(processed_corpus)
-        corpus_tfidf=create_tfidf_model(dictionary,processed_corpus)
-        keywords=keywords_by_lda(corpus_tfidf,dictionary)
-        docs=[]
-        for terms in keywords:
-            doc=keywords_to_docs_by_lda(terms,corpus,dictionary,corpus_tfidf,threshold=0)
-            docs+=[doc]
-        return keywords,docs
-
+def select_top_keywords(dictionary,corpus_tfidf,method,model=None,num_topics=5,num_terms=20):
+    if (method=='topic modeling'):
+        keywords=keywords_by_lda(corpus_tfidf,dictionary,num_topics,num_terms)
+    elif (method=='semantic modeling'):
+        # model=glove_model()
+        keywords=keywords_by_frequency(model,dictionary,num_topics,num_terms)
     else:
         print("Choose method from {'semantic modeling','topic modeling'}")
+    return keywords
 
         
 
+# sub_indices is the list of indices of the sub part of the corpus to work with
+# relative_indices is the result list of indices of the top docs (relative indices based on the sub corpus)
+def keywords_to_doc_list(terms,dictionary,corpus_tfidf,sub_indices='default',threshold=0):
+    if (sub_indices=='default'):
+        sub_indices=[i for i in range(0,7600)]
+    scores=[]
+    for i in range(0,len(corpus_tfidf)):
+        document=corpus_tfidf[i]
+        matched=0
+        summation=0
+        for term in terms:
+            tfidf=0
+            for idx,tfidf_score in document:
+                if (dictionary[idx]==term):
+                    matched+=1
+                    tfidf=tfidf_score
+                    break
+            summation+=tfidf
+        score=matched*summation
+        scores+=[score]
+    indices = (-np.array(scores)).argsort()
+    relative_indices=[idx for idx in indices if scores[idx]>threshold]
+
+    result=[sub_indices[idx] for idx in relative_indices]
+    return result
 
 
-# TODO
+
+
 # Match documents by keywords
 # score = match(keywords, document) x tf-idf, or x tf-idf^2
 # set a threshold, e.g. 0
 # input: keywords(a set of similar keywords), X is feature matrix
 # output: documents
-def keywords_to_docs(terms,corpus,vectorizer_or_dict,matrix,threshold=0):
-    if (method=='semantic modeling'):
-        return keywords_to_docs_by_frequency(terms,corpus,vectorizer_or_dict,matrix,threshold)
-    elif (method=='topic modeling'):
-        return keywords_to_docs_by_lda(terms,corpus,vectorizer_or_dict,matrix,threshold)
-    else:
-        print("Choose method from {'semantic modeling','topic modeling'}")
+def keywords_to_docs(corpus,doc_list):
+    top_docs=[]
+    for idx in doc_list:
+        top_docs+=[corpus[idx]]
+    return top_docs
 
 
 
-
-
+'''
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=os.environ.get('PORT', 3000), debug=True)
+'''
 
 
 
+def round(corpus,processed_corpus,document_list,method,model=None):
+    data = []
+    
+    sub_processed_corpus=[processed_corpus[idx] for idx in document_list]
+
+    dictionary=create_dictionary(sub_processed_corpus)
+    corpus_tfidf=create_tfidf_model(dictionary,sub_processed_corpus)
+    
+    keywords=select_top_keywords(dictionary,corpus_tfidf,method,model)
+
+    all_indices=[]
+    # Match documents by keywords
+    for terms in keywords:
+        indices=keywords_to_doc_list(terms,dictionary,corpus_tfidf,document_list,threshold=0)
+        top_doc=keywords_to_docs(corpus,indices)[:5]
+
+        item={
+            'words':terms,
+            'documents':top_doc,
+            'indices':indices
+        }
+        data.append(item)
+        all_indices.append(indices)        
+
+    write_to_txt(data,filename='iterative_frequency.txt')
+
+    return all_indices
+
+
+def iterative_decompose():
+    method='semantic modeling'
+    if (method=='semantic modeling'):
+        model=glove_model()
+    else:
+        model=None
+
+    corpus=retrieve_data()
+    processed_corpus=preprocess_using_spacy(corpus)
+
+    # round1
+    round1_document_list=[i for i in range(0,7600)]
+    round1_matched_list=round(corpus,processed_corpus,round1_document_list,method,model=model)
+        
+    # round2
+    round2_matched_list_on_topic0=round(corpus,processed_corpus,round1_matched_list[0],method,model=model)
+    round2_matched_list_on_topic1=round(corpus,processed_corpus,round1_matched_list[1],method,model=model)
+    round2_matched_list_on_topic2=round(corpus,processed_corpus,round1_matched_list[2],method,model=model)
+    round2_matched_list_on_topic3=round(corpus,processed_corpus,round1_matched_list[3],method,model=model)
+    round2_matched_list_on_topic4=round(corpus,processed_corpus,round1_matched_list[4],method,model=model)
+
+    # round3
+    round3_matched_list_on_topic0=round(corpus,processed_corpus,round2_matched_list_on_topic0[0],method,model=model)
