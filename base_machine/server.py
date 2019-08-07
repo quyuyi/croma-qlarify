58fb22329458bbb1fc6028be951ed6f748bff8ad
Yiwei Yang <yanyiwei@umich.edu>
2019-08-06 23:09:13 -0400

    fixed some bugs and went through 1 round of testing

--- base_machine/server.py
+++ base_machine/server.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
-import os
-from flask import Flask, render_template
-app = Flask(__name__)
+# import os
+# from flask import Flask, render_template
+# app = Flask(__name__)
 
 
-@app.route("/")
-def index():
-    return render_template('index.html')
+# @app.route("/")
+# def index():
+#     return render_template('index.html')
 
 '''
 if __name__ == "__main__":
@@ -41,9 +41,9 @@ import numpy as np
 # need change?
 def get_entropy(feature,dataset_indices='None'):
     if dataset_indices=='None':
-        dataset_indices=[i for i in range(0,feature)]
+        dataset_indices=[i for i in range(0,len(feature))]
 
-    # calculate entropy only based on the filtered dataset    
+    # calculate entropy only based on the filtered dataset
     filtered_dataset=[feature[i] for i in dataset_indices]
 
     labels=[]
@@ -63,7 +63,7 @@ def get_entropy(feature,dataset_indices='None'):
 # e.g.,  ['English', 'Français', '한국어/조선말', 'Español']
 # process the string read from csv file
 # to list object in python
-def get_feature_list(col_name):
+def get_feature_list(col_name, processed_dict):
     processed=[]
     for i in processed_dict[col_name]:
         list_i=ast.literal_eval(i)
@@ -76,13 +76,13 @@ def get_feature_list(col_name):
 
 # TODO
 # for empty field, should we keep it or filter it out?
-# return a list abosute indices
+# return a list of absolute indices
 # filtered_on_genres is the current 'live' datapoints
-def filter_dataset(feature_value,feature_list,filtered_on_genres='None'):
-    if (filtered_on_genres=='None'):
-        filtered_on_genres=[i for i in range(0,len(feature_list))]
+def filter_dataset(feature_value,feature_list,cur_points='None'):
+    if (cur_points=='None'):
+        cur_points=[i for i in range(0,len(feature_list))]
 
-    cur_dataset=[feature_list[i] for i in filtered_on_genres]
+    cur_dataset=[feature_list[i] for i in cur_points]
     filtered=[]
     if isinstance(cur_dataset[0],list):
         for index,ele in enumerate(cur_dataset):
@@ -94,7 +94,7 @@ def filter_dataset(feature_value,feature_list,filtered_on_genres='None'):
                 filtered+=[index]
     # for now filtered is a relative indices list
     # convert to an absolute indices list
-    abs_filtered=[filtered_on_genres[i] for i in filtered]
+    abs_filtered=[cur_points[i] for i in filtered]
     return abs_filtered
 
 
@@ -113,10 +113,10 @@ def test():
         processed_dict[col]=list(df[col])
 
     # extract only the name filed and throw away ids, etc.
-    processed_dict['genres']=get_feature_list('genres')
-    processed_dict['production_companies']=get_feature_list('production_companies')
-    processed_dict['production_countries']=get_feature_list('production_countries')
-    processed_dict['spoken_languages']=get_feature_list('spoken_languages')
+    processed_dict['genres']=get_feature_list('genres', processed_dict)
+    processed_dict['production_companies']=get_feature_list('production_companies', processed_dict)
+    processed_dict['production_countries']=get_feature_list('production_countries', processed_dict)
+    processed_dict['spoken_languages']=get_feature_list('spoken_languages', processed_dict)
 
     processed_collection=[]
     # ast.literal_eval requires input as dictionary in str type like '{}'
@@ -133,6 +133,8 @@ def test():
 
     ranked_indices=sorted(entropies.items(),key=lambda kv:kv[1],reverse=True)
 
+    print(ranked_indices)
+
     # ask the end user about the feature according to the ranking
 
     # end user may fail to answer many top feature
@@ -140,7 +142,8 @@ def test():
     # round2
 
     # finally able to filter on 'genres'
-    filtered_on_genres=filter_dataset('Action',processed_dict['genres'],filtered_on_genres='None')
+    filtered_on_genres=filter_dataset('Action',processed_dict['genres'],cur_points='None')
+    print(filtered_on_genres)
     # output
     # [5, 7, 8, 9, 14, 19, 22, 41, 43, 50, 53, 65, 69, 70, 85, 93, 96]
 
@@ -150,11 +153,21 @@ def test():
         r2_entropies[item] = get_entropy(processed_dict[item],dataset_indices=filtered_on_genres)
 
     r2_ranked_indices=sorted(r2_entropies.items(),key=lambda kv:kv[1],reverse=True)
-
+    print(r2_ranked_indices)
     # TODO
     # may need move away the features that are already asked previously
 
     # finally able to filter on 'spoken_languages'
-    filtered_on_language=filter_dataset('English',processed_dict['spoken_languages'],filtered_on_genres=filtered_on_genres)
+    filtered_on_language=filter_dataset('English',processed_dict['spoken_languages'],cur_points=filtered_on_genres)
+    # print(filtered_on_language)
     # output
     # [5, 7, 8, 9, 14, 19, 22, 41, 43, 53, 65, 69, 70, 85, 93, 96]
+    r3_entropies={}
+    for item in processed_dict:
+        r3_entropies[item] = get_entropy(processed_dict[item],dataset_indices=filtered_on_language)
+    r3_ranked_indices=sorted(r3_entropies.items(),key=lambda kv:kv[1],reverse=True)
+    # print(r3_ranked_indices)
+
+    filtered_on_prod_countries=filter_dataset('United States of America',processed_dict['production_countries'],cur_points=filtered_on_language)
+    print(filtered_on_prod_countries)
+    # [5, 7, 8, 9, 14, 19, 22, 41, 43, 53, 65, 69, 70, 85, 93]
